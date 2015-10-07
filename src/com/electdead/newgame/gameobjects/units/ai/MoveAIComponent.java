56ca50cf6e30c6f839384d9abd0e3e78ce50e9fd
Dmitry Onyshchenko <sacr8tum@gmail.com>
2015-10-07 10:41:55 +0300

    Added moveDirCorrectionTrigger

--- src/com/electdead/newgame/gameobjects/units/ai/MoveAIComponent.java
+++ src/com/electdead/newgame/gameobjects/units/ai/MoveAIComponent.java
@@ -1,9 +1,12 @@
 package com.electdead.newgame.gameobjects.units.ai;
 
+import com.electdead.newgame.engine.EngineV1;
 import com.electdead.newgame.gameobjects.units.Unit;
 import com.electdead.newgame.physics.Vector2F;
 
 public class MoveAIComponent extends AIComponent {
+    private int delayTimer = 0;
+    private int moveDirCorrectionTrigger = 250 / EngineV1.MS_PER_UPDATE;
 
     public MoveAIComponent(AIContainer aic, int priority) {
         super(aic, priority);
@@ -13,7 +16,7 @@ public class MoveAIComponent extends AIComponent {
     public void think(Unit unit) {
         if (unit.target == null) {
             unit.moveDir = unit.physModel.getMoveDir();
-        } else {
+        } else if (delayTimer++ > moveDirCorrectionTrigger) {
             Vector2F newDir = unit.target.pos.copy();
             newDir.sub(unit.pos);
             newDir.normalize();
