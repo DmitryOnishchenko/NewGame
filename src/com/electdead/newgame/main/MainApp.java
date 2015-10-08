752fa8674555ad56b4cb13c990a4341b91abf321
Dmitry Onyshchenko <sacr8tum@gmail.com>
2015-10-08 11:10:43 +0300

    MainApp refactoring

--- src/com/electdead/newgame/main/MainApp.java
+++ src/com/electdead/newgame/main/MainApp.java
@@ -1,29 +1,24 @@
 package com.electdead.newgame.main;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-
 import com.electdead.newgame.engine.AbstractGameLoop;
 import com.electdead.newgame.engine.EngineV1;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainApp {
-//	public static GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-//	public static int width = gd.getDisplayMode().getWidth();
-//	public static int height = gd.getDisplayMode().getHeight();
-	
-	public static final int WIDTH = 1280;
-	public static final int HEIGHT = 720;
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				JFrame gameWindow = new GameWindow("Test game v0.1", WIDTH, HEIGHT);
-				AbstractGameLoop engine = new EngineV1(WIDTH, HEIGHT);
-				gameWindow.add(engine);
-				gameWindow.setVisible(true);
-				engine.start();
-			}
-		});
-	}
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame gameWindow = new GameWindow("Test game v0.1", WIDTH, HEIGHT);
+                AbstractGameLoop engine = new EngineV1(WIDTH, HEIGHT);
+                gameWindow.add(engine);
+                gameWindow.setVisible(true);
+                engine.start();
+            }
+        });
+    }
 }
